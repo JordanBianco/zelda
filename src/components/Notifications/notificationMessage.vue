df26711b8c1b4d597a0192aefdbfb30e8f30b3f3
Jordan Bianco <jordan.bianco@outlook.it>
2022-02-23 18:18:13 +0100

    fix notification flash timeout

--- src/components/Notifications/notificationMessage.vue
+++ src/components/Notifications/notificationMessage.vue
@@ -29,7 +29,7 @@ export default {
     created() {
         this.timeout = setTimeout(() => {
             this.removeNotification();
-        }, 35000)
+        }, 3250)
     },
     beforeDestroy() {
         clearTimeout(this.timeout);
